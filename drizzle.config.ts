888887c398948c5bda2813bc7525067b4f390cf1
Dmytro Pashchenko <paschenkod2@gmail.com>
2025-10-31 14:47:00 +0100

    Fix drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,8 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  schema: './db/schema.ts',
-  out: './db/migrations',
+  schema: ['./src/**/schema.ts'],
+  out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: { url: process.env.DATABASE_URL! },
   casing: 'snake_case',
