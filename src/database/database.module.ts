28562f5ab7eb76456fd12a7a71d0af905fda0483
Dmytro Pashchenko <paschenkod2@gmail.com>
2025-10-31 15:18:49 +0100

    Import schemas in database.module.ts

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,6 +3,10 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { DATABASE_CONNECTION } from './database-connection';
 import { ConfigService } from '@nestjs/config';
 import { Pool } from 'pg';
+import * as eventsSchema from '../events/schema';
+import * as sportsSchema from '../sports/schema';
+import * as teamsSchema from '../teams/schema';
+import * as venuesSchema from '../venues/schema';
 
 @Module({
   providers: [
@@ -13,7 +17,12 @@ import { Pool } from 'pg';
           connectionString: configService.getOrThrow('DATABASE_URL'),
         });
         return drizzle(pool, {
-          schema: {},
+          schema: {
+            ...eventsSchema,
+            ...sportsSchema,
+            ...teamsSchema,
+            ...venuesSchema,
+          },
         });
       },
       inject: [ConfigService],
