0f14646ecca83154ab9c19dada071d3f732cc191
Dmytro Pashchenko <paschenkod2@gmail.com>
2025-11-03 14:16:05 +0100

    Add Swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document); // ← сюда передаём документ
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
   console.log('Server running on port ' + (process.env.PORT ?? 3000));
